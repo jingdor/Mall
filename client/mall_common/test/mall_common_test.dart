3e66feb72b2bd4b3692b2ae13afbfe5c3b753799
jzx <945627077@qq.com>
2022-01-17 14:14:16 +0800

    '增加eventbus'

--- client/mall_common/test/mall_common_test.dart
+++ client/mall_common/test/mall_common_test.dart
@@ -1,5 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mall_common/eventbus.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  test('测试eventbus', () {
+    var eb = new EventBus();
+
+    var callback = (source, payload) {
+      print("触发者" + source);
+      print("负载" + payload);
+    };
+
+    eb.on("test", "test", callback);
+
+    eb.fire("test1", "test", "hello");
+  });
 }
